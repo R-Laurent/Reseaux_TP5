ae0b1ffbe5560848215c4ec61e4a8559c0d24957
raphael <raphael.laurent@etu.univ-amu.fr>
2022-02-09 19:06:06 +0100

    9/02

--- src/Serveur_TCP.java
+++ src/Serveur_TCP.java
@@ -7,16 +7,18 @@ public class Serveur_TCP {
         ServerSocket s = new ServerSocket(1234);
         Socket c = s.accept();
         InputStream is = c.getInputStream();
-
+        int x = 0;
         while(true) {
             BufferedReader text = new BufferedReader(new InputStreamReader(is));
 
             String y = text.readLine();
+            System.out.println(y);
             if(y == null){
                 c.close();
                 s.close();
                 break;
             }
+            x++;
             System.out.println(y);
         }
 
